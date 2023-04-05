4fc4ffe4053ff96acc5a426b93811560a7e3d07d
canknbr <cankanbur1@gmail.com>
2023-04-05 12:31:31 +0300

    Initial Commit

---
+++ MachineLearningSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningSwift
+//
+//  Created by Can Kanbur on 5.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
